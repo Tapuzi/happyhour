823fd59aec2545b2420a40cc0dd1b98cdbc9b591
amitr <amit.r@bbumgames.com>
2022-01-27 22:50:21 +0200

    initial commit

---
+++ Assets/Scripts/NetworkManagerHappyHour.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class NetworkManagerHappyHour : NetworkManager
+{
+    public override void OnStartClient()
+    {
+        
+    }
+
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        NetworkPlayer player = conn.identity.GetComponent<NetworkPlayer>();
+        
+        player.SetDisplayName($"Player {numPlayers}");
+        player.SetColor();
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,20 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+        [SyncVar] [SerializeField] private string displayName = "MissingName";
+        [SyncVar] [SerializeField] private Color color;
+
+        [Server]
+        public void SetDisplayName(string newDisplayName)
+        {
+                displayName = newDisplayName;
+        }
+
+        [Server]
+        public void SetColor()
+        {
+                color = new Color(Random.Range(0, 1), Random.Range(0, 1),Random.Range(0, 1));
+        }
+}
