55685a9246cf886b021ec32546dcececc6e73d15
amitr <amitreuveni@gmail.com>
2022-01-28 16:16:13 +0200

    pickable

---
+++ Assets/Scripts/Pickable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public abstract class Pickable : MonoBehaviour
+    {
+        // Reference to the rigidbody
+        private Rigidbody2D rb;
+        public Rigidbody2D Rb => rb;
+        /// <summary>
+        /// Method called on initialization.
+        /// </summary>
+        private void Awake()
+        {
+            // Get reference to the rigidbody
+            rb = GetComponent<Rigidbody2D>();
+        }
+    }
+}
